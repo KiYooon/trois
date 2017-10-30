c9c05ab6944fef62c0d32f161e519161bbda286d
GD <GD@DESKTOP-3HIET8B>
2017-10-30 14:16:51 +0900

    이미지 파일

---
+++ src/main/java/kr/gudi/trois/controller/myroomController.java
@@ -0,0 +1,17 @@
+package kr.gudi.trois.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class myroomController {
+	
+//	 @RequestMapping("/myroom")
+//	   public ModelAndView myroom(ModelAndView mav) {
+//		 mav.setViewName("myroom");
+//		 return mav;
+//	   }
+}
